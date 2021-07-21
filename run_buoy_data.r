a0da49784c4b60916fbe9a3992c59bd858923b3a
Matthew Vaughan <mvaughan@lcbp.org>
2021-07-21 15:13:24 -0400

    Re-organizing and getting beta version of data site ready to publish

--- run_buoy_data.r
+++
@@ -1,9 +0,0 @@
-
-library(rmarkdown)
-
-render("data_site/data_site.Rmd")
-
-
-  # ftpUpload(what = "buoyPlots_v2.R",
-  #           to = "ftp://02d0c8c.netsolhost.com/test_r_script_upload.r",
-  #           userpwd = userpwd)
---
+++ run_lcbuoy_data.r
@@ -0,0 +1,15 @@
+
+lib_paths <- .libPaths()   # extract both paths
+new_paths <- c("C:/Users/mvaughan/Documents/R_package_library", lib_paths [2])  # change order
+.libPaths(new_paths)  # modify
+
+library(rmarkdown)
+library(rmdformats)
+
+render("lcbuoy_data.Rmd",
+       output_file = "data_site/index.html")
+
+
+  # ftpUpload(what = "buoyPlots_v2.R",
+  #           to = "ftp://02d0c8c.netsolhost.com/test_r_script_upload.r",
+  #           userpwd = userpwd)
