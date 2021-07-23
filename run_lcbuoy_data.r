c4e8653fc6f8e792f3c7779e9f2322a14324d2ce
Matthew Vaughan <mvaughan@lcbp.org>
2021-07-23 15:01:58 -0400

    Added Lake Champlain tributary data, several other tweaks

--- run_lcbuoy_data.r
+++ run_lcbuoy_data.r
@@ -1,11 +1,17 @@
+# Info ----------------------------------------------------------
+## Code written by Matthew C.H. Vaughan, Lake Champlain Basin Program
 
+# Setup ---------------------------
+# Set library path to local drive. 
 lib_paths <- .libPaths()   # extract both paths
 new_paths <- c("C:/Users/mvaughan/Documents/R_package_library", lib_paths [2])  # change order
 .libPaths(new_paths)  # modify
 
+# Load packages
 library(rmarkdown)
 library(rmdformats)
 
+# Render the RMarkdown file
 render("lcbuoy_data.Rmd",
        output_file = "index.html")
 
